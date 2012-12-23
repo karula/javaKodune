27d72b14c6009759514b47f32e5fc3b901bfb197
keito arula <keitoarula@gmail.com>
2012-12-23 16:08:57 +0200

    Võtsin tagasi lck faili kustutamise
    Muidu töötas

--- src/ee/projekt/web/MainController.java
+++ src/ee/projekt/web/MainController.java
@@ -10,8 +10,7 @@ public class MainController {
 	
 	@RequestMapping("/")
 	public String main(){			
-			File lck = new File("/usr/share/tomcat7/i377/Team07/db2.lck");
-			lck.delete();		
+			
 		return "index";
 	}
 }
